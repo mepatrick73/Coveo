6ce4dc4827de59a5a15949da0b690531abaca7b9
ThierryCantin-Demers <piertcd@gmail.com>
2024-01-13 15:12:54 -0500

    better float equality

--- Bot.cs
+++ Bot.cs
@@ -211,8 +211,7 @@ public class Bot
         var ownShipPosition = gameMessage.Ships[gameMessage.CurrentTeamId].WorldPosition;
 
         var actions = new List<Action>();
-        while (MathUtil.AngleBetween(ownShipPosition, enemyShip.WorldPosition) !=
-               MathUtil.AngleBetween(weaponToShootFrom.WorldPosition, enemyShip.WorldPosition))
+        while (Math.Abs(MathUtil.AngleBetween(ownShipPosition, enemyShip.WorldPosition) - MathUtil.AngleBetween(weaponToShootFrom.WorldPosition, enemyShip.WorldPosition)) < 1e-6f)
         {
             actions.Add(new ShipRotateAction(Math.Abs(MathUtil.AngleBetween(ownShipPosition, enemyShip.WorldPosition)
                                                       - MathUtil.AngleBetween(weaponToShootFrom.WorldPosition,
