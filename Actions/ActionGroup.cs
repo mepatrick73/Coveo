f9e3464934db24c201057852ac067d65de8842af
ThierryCantin-Demers <piertcd@gmail.com>
2024-01-13 10:52:01 -0500

    ActionGroup base

---
+++ Actions/ActionGroup.cs
@@ -0,0 +1,54 @@
+﻿
+
+using System.Numerics;
+
+namespace Application.Actions;
+
+public class ActionGroup
+{
+    protected ActionGroup()
+    {
+        Actions = new List<CustomAction>();
+    }
+    
+    public void Add(Action action)
+    {
+        Actions.Add(new CustomAction(action));
+    }
+    
+    public List<Action> GetActions()
+    {
+        return Actions.Select(a => a.Action).ToList();
+    }
+    private List<CustomAction> Actions { get; set; }
+}
+
+public class BreakShieldAction : ActionGroup
+{
+    public BreakShieldAction(GameMessage gameState, Vector shipPosition)
+    {
+        var weaponStations = gameState.Ships[gameState.CurrentTeamId].Stations.Turrets;
+        var mannedStations = weaponStations.Where(station => station.Operator != null).ToList();
+        
+        if (mannedStations.Count == 0)
+        {
+        }
+        
+        
+    }
+}
+
+public class ActionsGenerator
+{
+    public static List<ActionGroup> GenerateActions(GameMessage gameState)
+    {
+        var actions = new List<ActionGroup>();
+
+        foreach (var ship in gameState.ShipsPositions)
+        {
+            actions.Add(new BreakShieldAction(gameState, ship.Value));
+        }
+
+        return actions;
+    }
+}
---
+++ Actions/CustomAction.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Actions;
+
+public class CustomAction
+{
+    public CustomAction(Action action)
+    {
+        this.Action = action ?? throw new ArgumentNullException(nameof(action));
+    }
+    
+    public Action Action { get; set; }
+}
